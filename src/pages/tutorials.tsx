91ef820f09e67f00256284357e1505a916ed9347
Ozoku <dragon.djozoku@gmail.com>
2019-09-16 14:03:09 +0300

    wrote tutorial list page

--- src/pages/tutorials.tsx
+++ src/pages/tutorials.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
+import CodeImg from '../assets/code.jpg';
+import Tutorial from '../components/Tutorial';
+
+const tutorials = [
+  {
+    img: CodeImg,
+    title: 'Git Tutorial',
+    description: 'Quick tutorial of how to use Git.',
+    date: '10.9.2019 13:12',
+    time: '8:00'
+  },
+  {
+    img: CodeImg,
+    title: 'Git Tutorial',
+    description: 'Quick tutorial of how to use Git.',
+    date: '10.9.2019 13:12',
+    time: '8:00'
+  },
+  {
+    img: CodeImg,
+    title: 'Git Tutorial',
+    description: 'Quick tutorial of how to use Git.',
+    date: '10.9.2019 13:12',
+    time: '8:00'
+  },
+  {
+    img: CodeImg,
+    title: 'Git Tutorial',
+    description: 'Quick tutorial of how to use Git.',
+    date: '10.9.2019 13:12',
+    time: '8:00'
+  }
+];
 
 const TutorialsPage = () => {
-  return <>Hello</>;
+  return (
+    <>
+      <h2 className="text-4xl my-4 mx-24">Tutorials:</h2>
+      {tutorials.map(tutorial => (
+        <Tutorial {...tutorial} />
+      ))}
+    </>
+  );
 };
 
 export default TutorialsPage;
