db8affa2f5d5b317a04db56719d1d2f53c64a512
Ozoku <dragon.djozoku@gmail.com>
2019-09-16 14:03:39 +0300

    fix container y margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Container: React.FC = props => {
-  return <div className="container mx-auto px-4">{props.children}</div>;
+  return <div className="container mx-auto px-4 my-4">{props.children}</div>;
 };
 
 export default Container;
